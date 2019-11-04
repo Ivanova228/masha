d72223f2351713ef9711d0386c8a39767ce61635
Ivanova Maria <ivanova_228_maru@mail.ru>
2019-11-04 19:39:08 +0300

    first commit

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int args)
+{
+	setlocale(LC_ALL, "");
+	double x, y;
+	cout << "введите число x ";
+	cin >> x;
+	cout << "введите число y ";
+	cin >> y;
+	if (x / y > 0)
+	{
+		cout << log(x / y) + (pow(x, 2) - y * cos(x));
+	}
+	if (x / y < 0)
+	{
+		cout << log(fabs(x / y) + (pow(2.71, x + y)));
+	}
+	if (x / y == 0)
+	{
+		cout << sin(pow(y, 2) + 1);
+	}
+}
+
